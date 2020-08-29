b2ea955b31b41d0eef09f95c138fa993a6920365
youssef smimaa <youssef.smimaa@gmail.com>
2020-08-29 13:39:23 +0200

    Add TU for find product by id

--- src/test/java/com/service/product/service/ProductServiceImplTest.java
+++ src/test/java/com/service/product/service/ProductServiceImplTest.java
@@ -2,23 +2,33 @@ package com.service.product.service;
 
 import com.service.product.entity.Product;
 import com.service.product.repository.ProductRepository;
-import org.assertj.core.api.Assertions;
-import org.assertj.core.internal.bytebuddy.matcher.ElementMatchers;
-import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ProductServiceImplTest {
 
     @InjectMocks
-    private ProductService productService;
+    private ProductServiceImpl productService;
 
     @Mock
     private ProductRepository productRepository;
+
+    @BeforeEach
+    void init(){
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     void should_create_a_product() {
@@ -26,16 +36,30 @@ class ProductServiceImplTest {
         Product product=Product.builder().id(1L).title("P1").build();
 
         //When
-        Mockito.when(productRepository.save(product)).thenReturn(product);
+        when(productRepository.save(any(Product.class))).thenReturn(product);
 
         Product createdProduct = productService.create(product);
 
+        verify(productRepository,times(1)).save(any(Product.class));
+
         //Then
-        Assertions.assertThat(createdProduct).isEqualTo(product);
+        assertThat(createdProduct).isEqualTo(product);
     }
 
     @Test
-    void findProductById() {
+    void should_return_a_product_by_its_id() {
+        //Given
+        Product product=Product.builder().id(1L).title("P1").build();
+
+        //when
+        when(productRepository.findById(any())).thenReturn(Optional.of(product));
+
+        Product productById = productService.findProductById(1L);
+
+        verify(productRepository,times(1)).findById(any());
+
+        //Then
+        assertThat(productById).isEqualTo(product);
     }
 
     @Test
